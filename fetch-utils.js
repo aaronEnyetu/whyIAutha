c71ebff59d5f48e1a2547e3892d426cb091c258a
Aaron Enyetu <aaronenyetu@gmail.com>
2022-05-16 11:24:07 -0700

    Added Supabase URL and Key and created signup user function

--- fetch-utils.js
+++ fetch-utils.js
@@ -1,6 +1,6 @@
 // Enter Supabase Information
-const SUPABASE_URL = '';
-const SUPABASE_KEY = '';
+const SUPABASE_URL = 'https://wuqcwgxturbjhkdtdjwn.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind1cWN3Z3h0dXJiamhrZHRkanduIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTI3MjQ0MjMsImV4cCI6MTk2ODMwMDQyM30.qVkreSGtYAMaEk7THSwEY_1E0AB9q0iNMjuFXHaIa8Q';
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
@@ -8,7 +8,14 @@ export function getUser() {
     return client.auth.session() && client.auth.session().user;
 }
 
-export async function signupUser(email, password) {}
+
+//signs a new user in and puts an auth token in local storage in the browser
+export async function signupUser(email, password) {
+    const response = await client.auth.signUp({ email, password });
+    
+
+    return response.user;
+}
 
 export async function signInUser(email, password) {}
 
